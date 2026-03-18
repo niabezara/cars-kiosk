9705d1b74193516abf63cdb03213b78b1ba8f3c9
ilo-begashvili <ilo.begashvili@gmail.com>
2026-03-18 11:32:55 +0400

    Fix FlatList issue

--- components/car/Price.tsx
+++ components/car/Price.tsx
@@ -1,18 +1,19 @@
 import { Grade } from "@/types/types";
 import React from "react";
-import { FlatList } from "react-native";
+import { View } from "react-native";
 import PriceItem from "../ui/PriceItem";
 
 type PriceProps = {
   data: Grade[];
 };
 
-const renderItem = ({ item }: { item: Grade }) => <PriceItem option={item} />;
-const keyExtractor = (item: Grade) => item.id;
-
 const Price: React.FC<PriceProps> = ({ data }) => {
   return (
-    <FlatList data={data} keyExtractor={keyExtractor} renderItem={renderItem} />
+    <View>
+      {data.map((item) => (
+        <PriceItem key={item.id} option={item} />
+      ))}
+    </View>
   );
 };
 
